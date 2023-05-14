4761914f951e8c5261aaddd2d2b8647a8003fe46
Seth Woodson <sethwoodson3@gmail.com>
2023-05-14 17:22:53 -0500

    post added

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './ShelfPage.css';
+import { useDispatch } from 'react-redux';
 
 function ShelfPage() {
+  const dispatch = useDispatch();
   const [shelfList, setShelfList] = useState([]);
+  const [newItem, setNewItem] = useState({description: '', image_url: ''})
 
   useEffect(() => {
     fetchShelf();
   }, []);
+
 
   const fetchShelf = () => {
     axios.get('/api/shelf').then((response) => {
@@ -18,8 +22,29 @@ function ShelfPage() {
     });
   }
 
+  const handleNewItem = (key, value) => {
+    console.log('new item added');
+    setNewItem({...newItem, [key]: value})
+  }
+
+  const addItem = (event) => {
+    axios.post('/api/shelf').then((response) => {
+      event.preventDefault();
+      dispatch({ type: 'SEND_NEW_ITEM', payload: newItem});
+      setNewItem({description: '', image_url: ''})
+    })
+
+  }
+
   return (
     <div className="container">
+      <h2>Add Item</h2>
+      <pre>{JSON.stringify(newItem)}</pre>
+      <form onSubmit={addItem}>
+        <input type='text' value={newItem.description} placeholder='Description' onChange={(event) => handleNewItem('description', event.target.value)} />
+        <input type='text' value={newItem.image_url} placeholder='Image Url' onChange={(event) => handleNewItem('image_url', event.target.value)} />
+      </form>
+      <input type='submit' value='Add New Item' />
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
       {
